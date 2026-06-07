eac775c57aab3ce983b096e58cc846e2a027b7a6
Ankit <ankdev1998@gmail.com>
2026-06-08 00:59:51 +0530

    fix: add path require in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,9 @@
-const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 dotenv.config();
+const path = require('path');
 
 connectDB();
 
